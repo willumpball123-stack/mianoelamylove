fd0328fdb235e70aa0cec5821e6f8ec0015534d2
willumpball123-stack <willumpball123@gmail.com>
2026-02-10 14:36:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -46,7 +46,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "WAHOOO NOW CLICK THE BUTTON BELOWW",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
